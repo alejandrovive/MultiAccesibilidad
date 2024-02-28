29cda779e0a895350141397433ca51502408bac2
alejandrovive <alejandrove@alumnos.iesgalileo.es>
2024-02-28 20:21:12 +0100

    Subir

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Home from './Pages/Home';
+import Error from './Pages/Error';
+import Video from './Pages/Video';
+import '../src/Estilos/estilos.scss';
+import Menu from './Componentes/Menu';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './Componentes/Footer';
+function App() {
+
+  return (
+    <BrowserRouter>
+       <Menu />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/video' element={<Video />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        <Footer />
+    </ BrowserRouter>
+  )
+}
+
+export default App
